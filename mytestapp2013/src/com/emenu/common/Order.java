8045ef5d0bd9ae5a5e093b535ecf6cf815644a4e
lingpiao.kong@gmail.com <lingpiao.kong@gmail.com@c67ec07e-42f0-eb5b-086f-b3312ebee723>
2013-05-30 09:35:59 +0000

    add order

---
+++ mytestapp2013/src/com/emenu/common/Order.java
@@ -0,0 +1,72 @@
+package com.emenu.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.emenu.models.Dish;
+import com.emenu.models.OrderItem;
+
+public class Order {
+	private static Order instance = null;
+	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+	private Order() {
+	}
+
+	public static Order getInstance() {
+		if (instance == null) {
+			instance = new Order();
+		}
+		return instance;
+	}
+
+	public void add(OrderItem newItem) {
+		OrderItem ordered = find(newItem);
+		if (ordered != null) {
+			ordered.setAmount(ordered.getAmount() + newItem.getAmount());
+		} else {
+			orderItems.add(newItem);
+		}
+	}
+
+	public void add(Dish dish) {
+		OrderItem orderItem = new OrderItem(dish, 1);
+		add(orderItem);
+	}
+
+	public void add(Dish dish, int amount) {
+		OrderItem orderItem = new OrderItem(dish, amount);
+		add(orderItem);
+	}
+
+	public void remove(OrderItem orderItem, boolean clear) {
+		OrderItem ordered = find(orderItem);
+		if (ordered != null) {
+			if (clear || ordered.getAmount() == 1) {
+				orderItems.remove(ordered);
+			} else {
+				ordered.setAmount(ordered.getAmount() - 1);
+			}
+		}
+	}
+
+	public void remove(OrderItem orderItem) {
+		remove(orderItem, false);
+	}
+
+	private OrderItem find(OrderItem orderItem) {
+		if (orderItem == null) {
+			return null;
+		}
+		for (OrderItem item : orderItems) {
+			if (orderItem.equals(item)) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+}
