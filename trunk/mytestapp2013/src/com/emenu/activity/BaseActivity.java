7492081809a68fe8b308f56ac998b900ddbf9f71
lingpiao.kong@gmail.com <lingpiao.kong@gmail.com@c67ec07e-42f0-eb5b-086f-b3312ebee723>
2013-06-11 03:36:34 +0000

    Add scrollview for the specials

--- trunk/mytestapp2013/src/com/emenu/activity/BaseActivity.java
+++ trunk/mytestapp2013/src/com/emenu/activity/BaseActivity.java
@@ -63,7 +63,7 @@ public class BaseActivity extends Activity {
 		} else if (id == R.id.favorite) {
 			Intent intent = new Intent(BaseActivity.this, FavoriteList.class);
 			startActivity(intent);
-			finish();
+			//finish();
 		} else if (id == R.id.language) {
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setTitle("Chose your language");
